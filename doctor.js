5d202ad0c34039f265f2f5ccf9766a12a1c3d727
unknown <sidrahkhan0348@gmail.com>
2024-05-14 02:34:20 +0500

    first commit

---
+++ doctor.js
@@ -0,0 +1,21 @@
+// // // string
+// // // let str="Hello";
+// // // console.log(str.replace("lo","p"))
+
+// // //trim method(removes extra spaces from string)
+// // let str="     sidrah  khan";
+// // console.log(str.trim())
+  
+
+// // practice Q
+// let fullName=prompt("enter your full name without spaces");
+// let userName="@"+fullName+fullName.length;
+// console.log(userName)
+
+
+
+// arrays
+let  marksStudent_1=10;
+let marksStudent_2=20;
+let marksStudent_3 =40;
+let marksStudent_4 =30;
